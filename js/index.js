7900b831d80097a0af469878de21bd316787e998
ZKJGOLD <zkjcwx@163.com>
2018-03-23 14:16:01 +0800

    新增Table元素周期表布局以及li点击弹窗事件

--- js/index.js
+++ js/index.js
@@ -1,10 +1,8 @@
 window.onload = function () {
     (function () {
-        let length = 5*5*5;
-        let oUl = document.getElementById("list").children[0];
-        let aLi = oUl.children;
-
-
+        let length = 5*5*5,
+            oUl = document.getElementById("list").children[0],
+            aLi = oUl.children;
         //初始化
         (function () {
             for(let i=0;i<length;i++){
@@ -21,11 +19,73 @@ window.onload = function () {
                 oLi.style.transform = `translate3D(${tX}px,${tY}px,${tZ}px)`;
                 oUl.appendChild(oLi);
             }
-            setTimeout(Grid,1000);
+            setTimeout(Grid,200);
         })();
 
+        //li的点击弹窗事件
+        (function () {
+            let oAlert = document.getElementById("alert");
+            oUl.onclick = function (e) {
+                let target = e.target;
+                if(/li/i.test(target.nodeName)){
+                    target.zkj = target.zkj?false:show();
+                }
+                e.stopPropagation();
+            };
+            oAlert.onclick = function (e) {
+                e.stopPropagation();
+            };
+            document.onclick = function () {
+                hide();
+            };
+            function show() {
+                if(!oAlert.timer){
+                    oAlert.timer = true;
+                    oAlert.style.display = "block";
+                    oAlert.style.transform = `rotateY(${0}deg) scale(${2})`;
+                    oAlert.style.opacity = "0";
+                    let time = 300,
+                        sTime = new Date();
+                    function m() {
+                        let prop = (new Date - sTime)/time;
+                        if(prop>=1){
+                            prop = 1;
+                            oAlert.timer = false;
+                        }else{
+                            requestAnimationFrame(m);
+                        }
+                        oAlert.style.transform = `rotateY(${0}deg) scale(${(1-2)*prop+2})`;
+                        oAlert.style.opacity = prop;
+                    }
+                    requestAnimationFrame(m);
+                }
+                return false;
+            }
 
-
+            function hide() {
+                if(oAlert.style.display === "block" && !oAlert.timer){
+                    oAlert.timer = true;
+                    oAlert.style.display = "block";
+                    oAlert.style.transform = `rotateY(${0}deg) scale(${1})`;
+                    let time = 800,
+                        sTime = new Date();
+                    function m() {
+                        let prop = (new Date - sTime)/time;
+                        if(prop>=1){
+                            prop = 1;
+                            oAlert.timer = false;
+                            oAlert.style.display = "none";
+                        }else{
+                            requestAnimationFrame(m);
+                        }
+                        oAlert.style.transform = `rotateY(${180*prop}deg) scale(${1-prop})`;
+                        oAlert.style.opacity = 1-prop;
+                    }
+                    requestAnimationFrame(m);
+                }
+            }
+        })();
+        
         //拖拽滚轮事件的添加
         (function () {
             let roX = 0,
@@ -46,8 +106,13 @@ window.onload = function () {
                     lastX = sX,
                     lastY = sY,
                     x_ = 0,
-                    y_ = 0;
+                    y_ = 0,
+                    ifMove = false;
+                if(/li/i.test(e.target.nodeName)){
+                    var thisLi = e.target;
+                }
                 this.onmousemove = function (e) {
+                    ifMove = true;
                     x_ = e.clientX - lastX;
                     y_ = e.clientY - lastY;
 
@@ -59,6 +124,10 @@ window.onload = function () {
 
                 };
                 this.onmouseup = function (e) {
+                    console.log(thisLi);
+                    if(ifMove && e.target === thisLi){
+                        thisLi.zkj = true;
+                    }
                     this.onmousemove = null;
                     function m() {
                         x_ *= 0.9;
@@ -72,8 +141,6 @@ window.onload = function () {
                     timerMouse = requestAnimationFrame(m);
                 }
             };
-
-
             !function (fn) {
                 if(document.onmousewheel === undefined){
                     document.addEventerListener("DOMMouseScroll",function (e) {
@@ -105,54 +172,125 @@ window.onload = function () {
 
         //Table 元素周期表
         function Table() {
-
+            if(Table.arr){
+                for(let i=0;i<length;i++){
+                    aLi[i].style.transform = Table.arr[i];
+                }
+            }else {
+                Table.arr = [];
+                let n = Math.ceil(length/18)+2;
+                let midY = n/2 - 0.5,
+                    midX = 18/2 - 0.5,
+                    disX = 170,
+                    disY = 210;
+                let arr = [
+                    {x : 0 , y : 0},
+                    {x : 17 , y : 0},
+                    {x : 0 , y : 1},
+                    {x : 1 , y : 1},
+                    {x : 12 , y : 1},
+                    {x : 13 , y : 1},
+                    {x : 14 , y : 1},
+                    {x : 15 , y : 1},
+                    {x : 16 , y : 1},
+                    {x : 17 , y : 1},
+                    {x : 0 , y : 2},
+                    {x : 1 , y : 2},
+                    {x : 12 , y : 2},
+                    {x : 13 , y : 2},
+                    {x : 14 , y : 2},
+                    {x : 15 , y : 2},
+                    {x : 16, y : 2},
+                    {x : 17, y : 2},
+                ];
+                for(let i=0;i<length;i++){
+                    let x,y;
+                    if(i<18){
+                        x = arr[i].x;
+                        y = arr[i].y;
+                    }else{
+                        x = i%18;
+                        y = Math.floor(i/18)+2;
+                    }
+                    let val = `translate3D(${(x-midX)*disX}px,${(y-midY)*disY}px,${0}px)`;
+                    Table.arr[i] = val;
+                    aLi[i].style.transform = val;
+                }
+            }
         }
         //Sphere 球状布局
         function Sphere() {
-            let arr = [1,3,8,9,11,14,21,15,12,10,9,7,4,1],
-                arrLength = arr.length,
-                xDeg = 180/(arrLength-1);
-            for(let i=0;i<length;i++){
-                //求出当前i处于第几层 第几个
-                let numC = 0,numG = 0;//层数，个数
-                let arrSum = 0;
-                for(let j=0;j<arrLength;j++){
-                    arrSum += arr[j];
-                    if(arrSum>i){
-                        numC = j;
-                        numG = i-(arrSum-arr[j]);
-                        break;
-                    }
+            if(Sphere.arr){
+                for(let i=0;i<length;i++){
+                    aLi[i].style.transform = Sphere.arr[i];
                 }
-
-                let yDeg = 360/arr[numC];
-                aLi[i].style.transform = `rotateY(${(numG+1.3)*yDeg}deg) rotateX(${90-numC*xDeg}deg) translateZ(${800}px)`;
+            }else{
+                Sphere.arr = [];
+                let arr = [1,3,8,9,11,14,21,15,12,10,9,7,4,1],
+                    arrLength = arr.length,
+                    xDeg = 180/(arrLength-1);
+                for(let i=0;i<length;i++){
+                    //求出当前i处于第几层 第几个
+                    let numC = 0,numG = 0;//层数，个数
+                    let arrSum = 0;
+                    for(let j=0;j<arrLength;j++){
+                        arrSum += arr[j];
+                        if(arrSum>i){
+                            numC = j;
+                            numG = i-(arrSum-arr[j]);
+                            break;
+                        }
+                    }
+                    let yDeg = 360/arr[numC];
+                    let val = `rotateY(${(numG+1.3)*yDeg}deg) rotateX(${90-numC*xDeg}deg) translateZ(${800}px)`;
+                    Sphere.arr[i] = val;
+                    aLi[i].style.transform = val;
+                }
             }
+
         }
         //Grid层叠式布局
         function Grid() {
-            let disX = 300;//每一个li 水平（x）方向的间距
-            let disY = 350;//每一个li 垂直（y）方向的间距
-            let disZ = 500;//每一个li 纵深（z）方向的间距
+            if(Grid.arr){
+                for(let i=0;i<length;i++){
+                    aLi[i].style.transform = Grid.arr[i];
+                }
+            }else{
+                Grid.arr = [];
+                let disX = 300;//每一个li 水平（x）方向的间距
+                let disY = 350;//每一个li 垂直（y）方向的间距
+                let disZ = 500;//每一个li 纵深（z）方向的间距
 
-            for(let i=0;i<length;i++){
-                let oLi = aLi[i];
-                let x = (oLi.x - 2)*disX,
-                    y = (oLi.y - 2)*disY,
-                    z = (oLi.z - 2)*disZ;
-                oLi.style.transform = `translate3D(${x}px,${y}px,${z}px)`;
+                for(let i=0;i<length;i++){
+                    let oLi = aLi[i];
+                    let x = (oLi.x - 2)*disX,
+                        y = (oLi.y - 2)*disY,
+                        z = (oLi.z - 2)*disZ;
+                    let val = `translate3D(${x}px,${y}px,${z}px)`;
+                    Grid.arr[i] = val;
+                    oLi.style.transform = val;
+                }
             }
-        }
 
+        }
         //Helix螺旋式布局
         function Helix() {
-            let h = 4;
-            let num = Math.round(length/h);
-            let deg = 360/num;
-            let mid = Math.floor(length/2);
-            let tY = 6;
-            for(let i=0;i<length;i++){
-                aLi[i].style.transform = `rotateY(${i*deg}deg) translateY(${(i-mid)*tY}px) translateZ(${900}px)`
+            if(Helix.arr){
+                for(let i=0;i<length;i++){
+                    aLi[i].style.transform = Helix.arr[i];
+                }
+            }else{
+                Helix.arr = [];
+                let h = 4;
+                let num = Math.round(length/h);
+                let deg = 360/num;
+                let mid = Math.floor(length/2);
+                let tY = 6;
+                for(let i=0;i<length;i++){
+                    let val = `rotateY(${i*deg}deg) translateY(${(i-mid)*tY}px) translateZ(${900}px)`;
+                    Helix.arr[i] = val;
+                    aLi[i].style.transform = val;
+                }
             }
         }
     })();
